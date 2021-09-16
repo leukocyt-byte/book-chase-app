61ffdedd6c0b5964e61306411b3a6dfecd0cd060
Justyna <justynka.takk@gmail.com>
2021-09-16 17:49:31 +0200

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from 'react';
+import AuthorTable from './components/author/AuthorTable';
+
 import './styles.css';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [author, setAuthor] = useState({});
+
+  const search = (e) => {
+    if (e.key === 'Enter') {
+      fetch(`https://openlibrary.org/search/authors.json?q=${query}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setQuery('');
+          setAuthor(result);
+          console.log(result);
+        });
+    }
+  };
   return (
     <div className="app">
       <div className="col" id="col__left">
         AUTHORS
-        <input type="text" placeholder="SEARCH"></input>
+        <input
+          type="text"
+          placeholder="SEARCH"
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}
+        ></input>
+        {result === {} ? '' : <AuthorTable />}
       </div>
       <div className="col">BOOKS</div>
     </div>
