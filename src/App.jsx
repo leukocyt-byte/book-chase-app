684b0e83cb02942c20f873d346e016ce358598d5
Justyna <justynka.takk@gmail.com>
2021-09-16 18:00:38 +0200

    table results

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,6 @@ function App() {
           value={query}
           onKeyPress={search}
         ></input>
-        {result === {} ? '' : <AuthorTable />}
       </div>
       <div className="col">BOOKS</div>
     </div>
--- src/components/author/AuthorTable.jsx
+++ src/components/author/AuthorTable.jsx
@@ -1,21 +1,22 @@
 const AuthorTable = (props) => {
-  const { products } = props;
+  const { autors } = props;
   return (
     <table>
-      <caption>Our products</caption>
+      <caption>Author results</caption>
       <thead>
         <tr>
           <th>Name</th>
-          <th>Price</th>
-          <th>In Stock</th>
+          <th>Work count</th>
+          <th>Birth date</th>
+          <th>Top subjects</th>
         </tr>
       </thead>
       <tbody>
-        {products.map((product) => (
-          <tr key={product.id}>
-            <td>{product.name}</td>
-            <td>{product.price}</td>
-            <td>{product.stock}</td>
+        {autors.map((autor) => (
+          <tr key={autor.docs[0].key}>
+            <td>{autor.docs[0].name}</td>
+            <td>{autor.docs[0].birth_date}</td>
+            <td>{autor.docs[0].top_subcjects[0]}</td>
           </tr>
         ))}
       </tbody>
